6afd3090cbc617d7320ba142e17b0fbc75d4dfba
Saubhagya <saubhagyaranjanbehera64@gmail.com>
2025-05-14 19:20:24 +0530

    refactor: Update theme colors and add ProjectFilter enum
    Updated the theme colors in `Color.kt` with new values, likely generated from Material Theme Builder.
    Introduced a new enum class `ProjectFilter` in `ProjectFilter.kt` to define different project filtering options (All, Ongoing, Completed, Unpaid).
    Enabled dynamic color in `Theme.kt` by setting `dynamicColor` to `true`.

---
+++ app/src/main/java/com/saubh/planwise/data/ProjectFilter.kt
@@ -0,0 +1,8 @@
+package com.saubh.planwise.data
+
+enum class ProjectFilter(val title: String) {
+    ALL("All Projects"),
+    ONGOING("Ongoing"),
+    COMPLETED("Completed"),
+    UNPAID("Unpaid")
+}
